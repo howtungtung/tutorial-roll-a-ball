9dfb4564e862014920e3b773db7034b70b3107ea
jasontung <jasontung2214@gmail.com>
2022-02-06 22:26:48 +0800

    1. 完成專案

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;//宣告引用，為了可以使用InputSystem相關功能
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;//宣告(Declare)一個公開的速度參數，可透過檢視器(Inspector)修改移動速度
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;//宣告私有變數，用來關聯(Reference)RigidBody Component的記憶體位置(Address)
+    private int count;
+    private float movementX;//宣告私有變數，用來存放玩家輸入的X方向值
+    private float movementY;//宣告私有變數，用來存放玩家輸入的Y方向值
+
+    //物件被生成時會自動被系統呼叫一次
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();//抓取自身物件上的RigidBody Component的記憶體位置(Address)並關聯(Reference)在rb變數中
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    //實作(Implement)InputSystem要呼叫的方法(Method)，InputValue為InputSystem呼叫時會傳入的參數型態
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();//InputValue參數類別(Class)提供Get方法去讀取當前玩家X Y方向的輸入值
+        movementX = movementVector.x;//從二維向量本地變數讀取X軸方向，並存入movementX變數中
+        movementY = movementVector.y;//從二維向量本地變數讀取Y軸方向，並存入movementY變數中
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    //物理運算之前會被系統自動呼叫，所有物理相關的操作都要寫在這個方法(Method)裡面
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0, movementY);//宣告一個三維向量的本地變數，並把先前紀錄的movementX和movementY傳入進去
+        rb.AddForce(movement * speed);//透過rb變數去呼叫身上的RigidBody Component，透過AddForce方法施加一個方向的力讓物體移動，透過乘上speed變數控制施加力的大小
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;//count++;
+            SetCountText();
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //每一次幀(Frame)都會被呼叫一次
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);//對自己身上的Transform Component呼叫旋轉方法，透過Time.deltaTime去弭平不同幀率上表現誤差
+    }
+}
