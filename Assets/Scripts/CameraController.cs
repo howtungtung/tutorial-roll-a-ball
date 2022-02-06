6e7170356d3f1d78b43181cc76d2122b2aeb4b94
howtungtung <howtungtung@gmail.com>
2022-02-06 23:22:52 +0800

    補上註解

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset;
+    public GameObject player;//透過Inspector指定Player物件
+    private Vector3 offset;//紀錄初始的位移差
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offset = transform.position - player.transform.position;//計算當前攝影機與Player物件的位移差，並存放到offset變數中
     }
 
-    // Update is called once per frame
+    //在每次Update之後執行，可確保在所有Update中改變位置之後，再移動攝影機位置
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position + offset;//讓攝影機與玩家永遠保持一樣的位移差，製造出追隨的效果
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;//宣告引用，為了可以使用InputSystem相關功能
-using TMPro;
+using TMPro;//引用TextMeshPro這個套件相關功能
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;//宣告(Declare)一個公開的速度參數，可透過檢視器(Inspector)修改移動速度
-    public TextMeshProUGUI countText;
-    public GameObject winTextObject;
+    public float speed = 0;//宣告(Declare)一個類型為Float的公開變數，可透過檢視器(Inspector)修改移動速度
+    public TextMeshProUGUI countText;//宣告一個類型為TextMeshProUGUI的公開變數，可透過檢視器指定要關聯的物件
+    public GameObject winTextObject;//宣告一個類型為GameObject的公開變數，可透過檢視器指定要關聯的物件
 
     private Rigidbody rb;//宣告私有變數，用來關聯(Reference)RigidBody Component的記憶體位置(Address)
-    private int count;
+    private int count;//宣告一個類型為Int的變數，用來紀錄當前碰到的Pickup數量
     private float movementX;//宣告私有變數，用來存放玩家輸入的X方向值
     private float movementY;//宣告私有變數，用來存放玩家輸入的Y方向值
 
@@ -19,8 +19,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();//抓取自身物件上的RigidBody Component的記憶體位置(Address)並關聯(Reference)在rb變數中
-        SetCountText();
-        winTextObject.SetActive(false);
+        SetCountText();//呼叫該方法
+        winTextObject.SetActive(false);//關閉該物件
     }
 
     //實作(Implement)InputSystem要呼叫的方法(Method)，InputValue為InputSystem呼叫時會傳入的參數型態
@@ -31,12 +31,13 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;//從二維向量本地變數讀取Y軸方向，並存入movementY變數中
     }
 
+    //將當前Count值，更新至UI上
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+        countText.text = "Count: " + count.ToString();//傳入新的字串給UI顯示
+        if(count >= 12)//如果當前累計數量超過12以上，則開啟勝利文字物件
         {
-            winTextObject.SetActive(true);
+            winTextObject.SetActive(true);//開啟勝利文字物件
         }
     }
 
@@ -47,13 +48,14 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);//透過rb變數去呼叫身上的RigidBody Component，透過AddForce方法施加一個方向的力讓物體移動，透過乘上speed變數控制施加力的大小
     }
 
+    //當其他觸發器(Trigger)碰到此物件，開方法會被呼叫，並傳入碰到的物件Collider
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Pickup"))
+        if (other.gameObject.CompareTag("Pickup"))//如果碰到的Collider遊戲物件Tag為Pickup才做判斷
         {
-            other.gameObject.SetActive(false);
-            count = count + 1;//count++;
-            SetCountText();
+            other.gameObject.SetActive(false);//關閉碰到的物件
+            count = count + 1;//count++; 增加計次
+            SetCountText();//更新Count UI文字
         }
     }
 }
